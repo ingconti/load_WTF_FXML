98639945260f421a4035ae52413d51237cf7f49f
ing.conti <ingconti@ingconti.com>
2020-05-31 20:31:19 +0200

    added 3rdf WTF FXML

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,16 +1,28 @@
 package org.example;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * JavaFX App
  */
+
+/*
+*
+* comers form JAVA non modular using FXML
+*
+* added other methods to call from timer
+*
+* */
 public class App extends Application {
 
     private static Scene scene;
@@ -31,7 +43,34 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
+
+    static void LoadWTFOnTimer() {
+        TimerTask task = new TimerTask() {
+
+            public void run() {
+
+                Platform.runLater(() -> {
+                    try {
+                        System.out.println("loading..");
+                        setRoot("wtf");
+
+                    } catch (Exception  ex) {
+                        System.out.println(ex);
+                    }
+                });
+            }
+        };
+
+        Timer timer = new Timer("Timer");
+
+        long delay = 3000L;
+        timer.schedule(task, delay);
+    }
+
+
     public static void main(String[] args) {
+
+        LoadWTFOnTimer();
         launch();
     }
 
