f0a040bb6a105072198f8f543f62db2f714989ae
ing.conti <ingconti@ingconti.com>
2022-05-31 17:08:31 +0200

    added loading IMG, too.

--- src/main/java/org/example/PrimaryController.java
+++ src/main/java/org/example/PrimaryController.java
@@ -2,11 +2,18 @@ package org.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
 
 public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+
+        // eventually load  image, too:
+        Image img =
+                new Image("full_0000s_0000_god_and_hero_cards_0056_scylla.png");
+
+
+        App.setRoot("AAA/secondary");
     }
 }
